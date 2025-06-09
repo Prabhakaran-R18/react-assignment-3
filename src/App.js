6e80d6dae120d701fc82bd3ad7aebf464a1e8489
Prabhakaran <prabhamalathi5@gmail.com>
2025-06-09 22:39:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from './components/Counter';
+import Timer from './components/Timer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to React Assignment 3</h1>
+      <Counter />
+      <Timer />
     </div>
   );
 }
